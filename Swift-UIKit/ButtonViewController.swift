9a1b32d0d66fb2a2953f016a46430812c5b15742
k2hoon <tayesymbol@gmail.com>
2023-05-26 22:30:03 +0900

    init and first commit.

---
+++ Swift-UIKit/ButtonViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ButtonViewController.swift
+//  Swift-UIKit
+//
+//  Created by k2hoon on 2023/01/10.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        print("hello buttons...")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift-UIKit/HomeTabViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTabViewController.swift
+//  Swift-UIKit
+//
+//  Created by k2hoon on 2023/05/01.
+//
+
+import UIKit
+
+class HomeTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        let basicVC = BasicViewController()
+//        let testVC = TestViewController()
+//        let advancedVC = AdvancedViewController()
+//        let settingsVC = SettingsViewController()
+//
+//        basicVC.tabBarItem = UITabBarItem(title: "Basic", image: UIImage(systemName: "square.text.square"), tag: 0)
+//        advancedVC.tabBarItem = UITabBarItem(title: "Advanced", image: UIImage(systemName: "square.3.layers.3d"), tag: 1)
+//        testVC.tabBarItem = UITabBarItem(title: "Test", image: UIImage(systemName: "hammer"), tag: 2)
+//        settingsVC.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 3)
+
+//        self.view.backgroundColor = .white
+//        self.viewControllers = [basicVC, advancedVC, testVC]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
