d132163b1d8a3e734f38019b3b541092362a2753
Kyung Hoon Kim <tayesymbol@gmail.com>
2023-06-11 04:22:05 +0900

    Update project basic view configuration. (#1)

---
+++ Swift-UIKit/BasicCollectionView.swift
@@ -0,0 +1,92 @@
+//
+//  BasicCollectionView.swift
+//  Swift-UIKit
+//
+//  Created by k2hoon on 2023/06/11.
+//
+
+import UIKit
+
+extension UIControl {
+    func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping()->()) {
+        addAction(UIAction { (action: UIAction) in closure() }, for: controlEvents)
+    }
+}
+
+class BasicCollectionView: UIViewController {
+    private let scrollView = UIScrollView()
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = 20
+        view.distribution = .fillEqually
+        
+        return view
+    }()
+    
+    enum ViewType: Int, CaseIterable {
+        case text
+        case button
+        
+        var viewValue: (title: String, vc: UIViewController) {
+            switch self {
+            case .text: return ("Text test", TextViewController())
+            case .button: return ("Button test", ButtonViewController())
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        self.view.addSubview(scrollView)
+        
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        
+        self.scrollView.addSubview(self.stackView)
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.stackView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            self.stackView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            self.stackView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+            self.stackView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor),
+        ])
+        
+        ViewType.allCases.forEach { viewType in
+            let viewValue = viewType.viewValue
+            let button = UIButton()
+            self.stackView.addArrangedSubview(button)
+            
+            // button custom
+            button.setTitle(viewValue.title, for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            button.backgroundColor = .white
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.black.cgColor
+            button.layer.cornerRadius = 32
+            button.layer.shadowColor = UIColor.gray.cgColor
+            button.layer.shadowOpacity = 1.0
+            button.layer.shadowOffset = .init(width: 10, height: 10)
+            button.layer.shadowRadius = 6
+            button.layer.masksToBounds = false
+            
+            // button action
+            button.addAction {
+                let presentVC = viewValue.vc
+                self.navigationController?.pushViewController(presentVC, animated: true)
+            }
+            
+            // button layout
+            button.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalToConstant: 300),
+                button.heightAnchor.constraint(equalToConstant: 400),
+            ])
+        }
+    }
+}
--- Swift-UIKit/BasicViewController.swift
+++ Swift-UIKit/BasicViewController.swift
@@ -8,20 +8,88 @@
 import UIKit
 
 class BasicViewController: UIViewController {
-
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 20
+        view.distribution = .fillEqually
+        
+        return view
+    }()
+    
+    lazy var basicCollections = BasicCollectionView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configure()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configure() {
+        self.configScrollView()
+        self.configContentView()
+        self.configButtonsStack()
     }
-    */
-
+    
+    private func configScrollView() {
+        self.view.addSubview(self.scrollView)
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            self.scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
+    
+    private func configContentView() {
+        self.scrollView.addSubview(self.contentView)
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let contentHeight = self.contentView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor)
+        contentHeight.priority = UILayoutPriority(250)
+        contentHeight.isActive = true
+        
+        NSLayoutConstraint.activate([
+            self.contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            self.contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            self.contentView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            self.contentView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+            self.contentView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor),
+        ])
+        
+        self.contentView.addSubview(self.stackView)
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.stackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            self.stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+        ])
+    }
+    
+    private func configButtonsStack() {
+        if let view = self.basicCollections.view {
+            self.addChild(self.basicCollections)
+            self.stackView.addArrangedSubview(view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                view.topAnchor.constraint(equalTo: self.stackView.topAnchor),
+                view.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor),
+                view.trailingAnchor.constraint(equalTo: self.stackView.trailingAnchor),
+                view.bottomAnchor.constraint(equalTo: self.stackView.bottomAnchor),
+            ])
+        }
+    }
+    
+    //    /*
+    //    // MARK: - Navigation
+    //
+    //    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        // Get the new view controller using segue.destination.
+    //        // Pass the selected object to the new view controller.
+    //    }
+    //    */
 }
